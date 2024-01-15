87917afb2441b8df97d6fd7f62966b03dbfa9383
Morgan <morgan124.bucher@gmail.com>
2024-01-15 15:17:06 +0100

    add top bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-route.module';
+import { TopBarComponent } from "./shared/top-bar/top-bar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
